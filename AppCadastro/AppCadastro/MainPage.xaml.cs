85bc620cc9c6f7dd279927c063d742b010072ae1
Ricardo Takuno <takunoricardo@gmail.com>
2017-08-09 22:23:45 -0300

    asAFasf

---
+++ AppCadastro/AppCadastro/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCadastro
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+		{
+			InitializeComponent();
+        }
+        private void btnCadastrar(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            DisplayAlert("SUCESSO", string.Format("O cliente ABC foi cadastrado com o e-mail E-mail@", veiculo.Nome, veiculo.PrecoFormatado), "ok");
+        }
+	}
+}
